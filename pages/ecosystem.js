3e62cde44af576831c2620c0c0a4f61e229f78f8
ifeoluwaayo <adeayomide112@gmail.com>
2022-06-25 19:05:38 +0100

    init

---
+++ pages/ecosystem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ecosystem = () => {
+	return <div>ecosystem</div>;
+};
+
+export default ecosystem;
---
+++ pages/tokenomics.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const tokenomics = () => {
+	return <div>tokenomics</div>;
+};
+
+export default tokenomics;
