1c65b4d55b9961ac957e426f9914bbd2b938f9ed
ifeoluwaayo <adeayomide112@gmail.com>
2022-07-03 15:40:33 +0100

    404 again

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,17 @@
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 export default function Custom404() {
+	const { t } = useTranslation();
+
 	return <div>Redirecting...</div>;
 }
 
-export const getStaticProps = () => {
+export const getStaticProps = async ({ locale }) => {
 	return {
+		props: {
+			...(await serverSideTranslations(locale ?? "en", ["common"])),
+		},
 		redirect: {
 			destination: "/",
 		},
